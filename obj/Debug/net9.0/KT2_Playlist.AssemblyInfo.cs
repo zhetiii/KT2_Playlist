aeae392c16a0df092e2e5e567050fb9781ffd2c2
Evgeniya <dikolenkoev24@st.ithub.ru>
2026-09-10 11:46:15 +0300

    Добавлен README

--- obj/Debug/net9.0/KT2_Playlist.AssemblyInfo.cs
+++ obj/Debug/net9.0/KT2_Playlist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KT2_Playlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0336725c3bf6c4cb298621c2c83e98b1d2f5d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b8b44ba08c1807ab7897dc246d8fc4a4faa597")]
 [assembly: System.Reflection.AssemblyProductAttribute("KT2_Playlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KT2_Playlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
